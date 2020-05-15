d80189bf6760129f8e9c38b918ecc1902fa50205
Priscila Vivian <priscila.vivian@avenuecode.com>
2020-05-15 18:10:03 -0300

    Tests now pass numbers as strings and not doubles

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,6 +5,7 @@ import pages.CalculatorPage;
 
 import java.net.MalformedURLException;
 import java.util.LinkedList;
+import java.util.List;
 
 public class CalculatorTest extends BaseTest {
 
@@ -34,11 +35,11 @@ public class CalculatorTest extends BaseTest {
     @Test
     public void testSubtraction() {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
-        LinkedList<Double> numbers = new LinkedList<>();
-        numbers.add(7.0);
-        numbers.add(4.0);
-        numbers.add(5.0);
-        numbers.add(3.0);
+        LinkedList<String> numbers = new LinkedList<>();
+        numbers.add("7");
+        numbers.add("4");
+        numbers.add("5");
+        numbers.add("3");
         String result = calculatorPage.calculate(numbers, "subtraction");
 
         Assert.assertEquals(result, "−5");
@@ -47,10 +48,10 @@ public class CalculatorTest extends BaseTest {
     @Test
     public void testDivision() {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
-        LinkedList<Double> numbers = new LinkedList<>();
-        numbers.add(8.0);
-        numbers.add(4.0);
-        numbers.add(2.0);
+        LinkedList<String> numbers = new LinkedList<>();
+        numbers.add("8");
+        numbers.add("4");
+        numbers.add("2");
         String result = calculatorPage.calculate(numbers, "division");
 
         Assert.assertEquals(result, "1");
@@ -59,9 +60,9 @@ public class CalculatorTest extends BaseTest {
     @Test
     public void testDivisionByZero() {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
-        LinkedList<Double> numbers = new LinkedList<>();
-        numbers.add(8.0);
-        numbers.add(0.0);
+        LinkedList<String> numbers = new LinkedList<>();
+        numbers.add("8");
+        numbers.add("0");
         String result = calculatorPage.calculate(numbers, "division");
 
         Assert.assertEquals(result, "Can't divide by 0");
@@ -70,10 +71,10 @@ public class CalculatorTest extends BaseTest {
     @Test
     public void testMultiplication() {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
-        LinkedList<Double> numbers = new LinkedList<>();
-        numbers.add(5.0);
-        numbers.add(4.0);
-        numbers.add(1.0);
+        LinkedList<String> numbers = new LinkedList<>();
+        numbers.add("5");
+        numbers.add("4");
+        numbers.add("1");
         String result = calculatorPage.calculate(numbers, "multiplication");
 
         Assert.assertEquals(result, "20");
@@ -82,10 +83,10 @@ public class CalculatorTest extends BaseTest {
     @Test
     public void testDecimalNumbers() {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
-        LinkedList<Double> numbers = new LinkedList<>();
-        numbers.add(5.3);
-        numbers.add(4.7);
-        numbers.add(1.5);
+        LinkedList<String> numbers = new LinkedList<>();
+        numbers.add("5.3");
+        numbers.add("4.7");
+        numbers.add("1.5");
         String result = calculatorPage.calculate(numbers, "sum");
 
         Assert.assertEquals(result, "11.5");
@@ -94,13 +95,12 @@ public class CalculatorTest extends BaseTest {
     @Test
     public void testDeleteFunction() {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
-        calculatorPage.selectNumber(2.0);
-        for (int i = 0; i < 3; i++)
-            calculatorPage.selectOperator("delete");
-        calculatorPage.selectNumber(5.0);
+        calculatorPage.selectNumber("2");
+        calculatorPage.selectOperator("delete");
+        calculatorPage.selectNumber("5");
         String onScreenContent = calculatorPage.getOnScreenContent();
 
-        Assert.assertEquals(onScreenContent, "5.0");
+        Assert.assertEquals(onScreenContent, "5");
     }
 
     @Test
@@ -123,11 +123,11 @@ public class CalculatorTest extends BaseTest {
     }
 
     private String genericSum(CalculatorPage calculatorPage) {
-        LinkedList<Double> numbers = new LinkedList<>();
-        numbers.add(7.0);
-        numbers.add(4.0);
-        numbers.add(5.0);
-        numbers.add(3.0);
+        LinkedList<String> numbers = new LinkedList<>();
+        numbers.add("7");
+        numbers.add("4");
+        numbers.add("5");
+        numbers.add("3");
         return calculatorPage.calculate(numbers, "sum");
     }
 }
--- src/test/java/pages/CalculatorPage.java
+++ src/test/java/pages/CalculatorPage.java
@@ -13,7 +13,7 @@ public class CalculatorPage extends BasePage {
         super(driver);
     }
 
-    public String calculate(Queue<Double> numbers, String operation) {
+    public String calculate(Queue<String> numbers, String operation) {
         if (numbers.size() > 0) {
             MobileElement operator = findOperator(operation);
             MobileElement equalsOperator = findOperator("equals");
@@ -26,8 +26,8 @@ public class CalculatorPage extends BasePage {
         return "0";
     }
 
-    public void selectNumber(double number) {
-        char[] characters = String.valueOf(number).toCharArray();
+    public void selectNumber(String number) {
+        char[] characters = number.toCharArray();
         for (char character : characters) {
             driver.findElement(By.id(CharacterConverter.getId(character))).click();
         }
@@ -46,7 +46,7 @@ public class CalculatorPage extends BasePage {
         return findOperator("formula").getText();
     }
 
-    private void doOperationOnList(Queue<Double> numbers, MobileElement operator) {
+    private void doOperationOnList(Queue<String> numbers, MobileElement operator) {
         selectNumber(numbers.remove());
         if (numbers.size() >= 1) {
             operator.click();
